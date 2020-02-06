3e7d0b09ddee5afc5826e930961e9dfeebe02789
Yujie <541298530@qq.com>
2020-02-06 17:11:12 +0800

    添加当前周spinner的初始化，以及监听器注册

--- app/src/main/java/com/project/fragment/timeTableFragment.java
+++ app/src/main/java/com/project/fragment/timeTableFragment.java
@@ -1,13 +1,13 @@
 package com.project.fragment;
 
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -16,27 +16,57 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.project.timetabletest.MainActivity;
 import com.project.timetabletest.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class timeTableFragment extends Fragment {
 
-    TableLayout courseTable;
+    private TableLayout courseTable;
+    private Spinner currentWeekSpinner;
+    private int currentWeek;
     public static ArrayList<ArrayList<TextView>>  courseArray;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        View view = inflater.inflate(R.layout.table_fragment,container,false);
        courseArray = new ArrayList<>();
+       initCurrentSpinner(view);
        bindViewToArray(view);
        upDateTimeTable();
         return view;
     }
 
-    /*
+    /**
+     * 初始化选择第几周的spinner 默认周数为1~30周
+     */
+    private void initCurrentSpinner(View view){
+        currentWeekSpinner = (Spinner) view.findViewById(R.id.id_spinner_week);
+        currentWeek = 1;
+        List<String> weekList= new ArrayList<String>();
+        for (int i = 1 ; i <= 30 ; i++){
+            weekList.add(Integer.toString(i));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,weekList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        currentWeekSpinner.setAdapter(adapter);
+        currentWeekSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String str_week=parent.getItemAtPosition(position).toString();
+                currentWeek = Integer.parseInt(str_week);
+                upDateTimeTable();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    /**
       将CourseTable.xml中的各个课程位置的textview放入到MainActivit中arrary中方便后续操作
      */
     private void bindViewToArray(View view){
@@ -55,7 +85,7 @@ public class timeTableFragment extends Fragment {
         }
     }
 
-    /*
+    /**
     根据数据库中的数据更新
      */
     private void upDateTimeTable(){
@@ -68,7 +98,7 @@ public class timeTableFragment extends Fragment {
                 if (j == courseArray.get(i).size()-1)
                 {
                     TextView temp = courseArray.get(i).get(j);
-                    temp.setText("专业综合课程设计");
+                    temp.setText("专业综合课程设计"+currentWeek);
                     temp.setBackgroundResource(R.drawable.coursetable_courseitem_border);
                 }
                 else
